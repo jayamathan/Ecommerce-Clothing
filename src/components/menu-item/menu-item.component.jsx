d3c7d69b3e9014c45fd59ec5115a35cdec0d8a84
jayamathan <jayamathanskj03@gmail.com>
2019-09-07 20:56:55 +0530

    'Broke homepage into components & add Effect to bg image

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./menu-item.style.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className={`${size} menu-item`}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
