a71ba16ac28067dc4e25ea1e3f0d43255945531d
jayamathan <jayamathanskj03@gmail.com>
2019-09-08 09:21:08 +0530

    Update men-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./homepage.style.scss";
 import Directory from "../../components/directory/directory.component";
+
 const HomePage = () => {
   return (
     <div className="homepage">
