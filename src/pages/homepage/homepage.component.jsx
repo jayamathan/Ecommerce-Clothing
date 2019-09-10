09d18a5bf394f11ffe33b0666180c82c787dc9da
jayamathan <jayamathanskj03@gmail.com>
2019-09-10 18:50:59 +0530

    Add Header component & Collection Preview

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import "./homepage.style.scss";
-import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => {
-  return (
-    <div className="homepage">
-      <Directory />
-    </div>
-  );
-};
+import Directory from "../../components/directory/directory.component";
+import "./homepage.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
 
 export default HomePage;
